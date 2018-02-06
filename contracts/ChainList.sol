d0f5e4ced15ab8f1494baf60c9a94a1d7c84ea7e
Juan <jcc.utn@gmail.com>
2018-02-05 23:22:53 -0600

    Added functionality to buy multiple articles. New file added (truffle_cmd_scripts.txt) with truffle console useful commands.

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,21 +1,30 @@
 pragma solidity ^0.4.11;
 
 contract ChainList{
+    // Custom types
+    struct Article{
+        uint id;
+        address seller;
+        address buyer;
+        string name;
+        string description;
+        uint256 price;
+    }
+
   //State variables
-  address seller;
-  address buyer;
-  string name;
-  string description;
-  uint256 price;
+  mapping (uint => Article) public articles;
+  uint articleCounter;
 
 
   // events
     event sellArticleEvent(
-      address indexed aSeller,
-      string aName,
-      uint256 aPrice
+        uint indexed id,
+          address indexed aSeller,
+          string aName,
+          uint256 aPrice
     );
     event buyArticleEvent(
+        uint indexed id,
         address indexed aSeller,
         address indexed aBuyer,
         string aName,
@@ -26,43 +35,80 @@ contract ChainList{
       //sell an article
       function sellArticle(string aName, string aDescription, uint256 aPrice)
       public {
-          seller = msg.sender;
-          name = aName;
-          description = aDescription;
-          price = aPrice;
-          sellArticleEvent(seller, name, price);
+          // a new article
+          articleCounter++;
+
+          // store this article
+          articles[articleCounter] = Article({
+              id:articleCounter,
+              seller: msg.sender,
+              buyer: 0x0,
+              name: aName,
+              description: aDescription,
+              price: aPrice
+              });
+
+
+          sellArticleEvent(articleCounter, msg.sender, aName, aPrice);
       }
 
-      // get article
-        function getArticle() public constant returns (
-          address aSeller,
-          address aBuyer,
-          string aName,
-          string aDescription,
-          uint256 aPrice) {
-            return (seller, buyer, name, description, price);
+      function getNumberOfArticles() public constant returns (uint) {
+          return articleCounter;
+      }
+
+      // fetch and returns all article IDs available for sale
+      function getArticlesForSale() public constant returns (uint[]) {
+          // we check whether there is at least one article
+          require(articleCounter > 0);
+
+          // prepare output arrays
+          uint[] memory articleIds = new uint[](articleCounter);
+
+          uint numberOfArticlesForSale = 0;
+          // iterate over articles
+          for (uint i = 1; i <= articleCounter; i++) {
+              // keep only the ID fot the article not already sold
+              if (articles[i].buyer == 0x0){
+                  articleIds[numberOfArticlesForSale] = articles[i].id;
+                  numberOfArticlesForSale++;
+              }
           }
 
-      function buyArticle() payable public {
-          // we check wether there is an article for sale
-          require(seller != 0x0);
+          // copy the articleIds array into the smaller forSale array
+          uint[] memory forSale = new uint[](numberOfArticlesForSale);
+          for (uint j = 0; j < numberOfArticlesForSale; j++){
+              forSale[j] = articleIds[j];
+          }
+          return (forSale);
+      }
 
-          // we check that the article was not already sold
-          require(buyer == 0x0);
+      // buy an article
+      function buyArticle(uint anId) payable public {
+          // we check wether there is at least one article
+          require(articleCounter > 0);
 
-          // we dont allow the seller to buy its own article
-          require(msg.sender != seller);
+          // we check whethere the article exists
+          require(anId > 0 && anId <= articleCounter);
 
-          // we check wethere the value sent corresponds to the article price
-          require(msg.value == price);
+          // we retreive the article from the list
+          Article storage article = articles[anId];
+
+          // we check if the article has not already been sold
+          require(article.buyer == 0x0);
+
+          // we dont allow the seller to buy it's own article
+          require(article.seller != msg.sender);
+
+          // we check whether the value sent corresponds to the article price
+          require(article.price == msg.value);
 
           // keep buyer's information
-          buyer = msg.sender;
+          article.buyer = msg.sender;
 
           // the buyer can buy the article
-          seller.transfer(msg.value);
+          article.seller.transfer(msg.value);
 
           // trigger the event
-          buyArticleEvent(seller, buyer, name, price);
+          buyArticleEvent(anId, article.seller, article.buyer, article.name, article.price);
       }
 }
