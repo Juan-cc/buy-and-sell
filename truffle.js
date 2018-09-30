dd58d0ebf6d0c890c5bcc7adbe89ca4f1d20ae0d
Juan Castellon <jcc.utn@gmail.com>
2018-09-29 21:59:50 -0600

    Rinkeby network added

--- truffle.js
+++ truffle.js
@@ -6,6 +6,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+      },     
+      rinkeby: {
+        host: "localhost",
+        port: 8545,
+        network_id: 4, // Rinkeby default test network
+        gas: 4700000
     }
   }
 };
